ab253c9f6dc6190b71a823dc560e511ffbd88a11
s1120289-lgtm <s1120289@dmhs.kh.edu.tw>
2025-09-15 03:30:59 +0000

    第一次提交

---
+++ test1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ int main() {
+ int year = 2025;                  
+  double price = 10.99;             
+  double gpa = 2.5, temperature = 25.1; 
+  std::cout << "year=" << year << "\n";
+  std::cout << "price=" << price << "\n";
+   std::cout << "gpa=" << gpa << ", temp=" << temperature << "\n";
+   return 0;
+}
+   
