bdd87cee02feb6f0ccc8aa9caa4711de0572d8f3
s1120289-lgtm <s1120289@dmhs.kh.edu.tw>
2025-09-22 03:23:58 +0000

    第二次提交

--- test1/main.cpp
+++ test1/main.cpp
@@ -8,4 +8,18 @@
    std::cout << "gpa=" << gpa << ", temp=" << temperature << "\n";
    return 0;
 }
-   
+   #include <iostream>
+
+int main() {
+    double celsius;
+    double fahrenheit;
+
+    std::cout << "請輸入華氏溫度!\n";
+    std::cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) / 9 * 5;
+
+    std::cout << "攝氏溫度為: " << celsius << " 度\n";
+    
+    return 0;
+}
